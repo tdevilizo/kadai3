a7cefbd43520c41ce7e743ab5e72751d8e324076
tdevilizo <145487057+tdevilizo@users.noreply.github.com>
2023-10-02 23:44:07 +0900

    Update Main.java

--- kadai3/src/main/java/org/example/Main.java
+++ kadai3/src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String,Integer>resultMap = new HashMap<String,Integer>();
+        Map<String, Integer> resultMap = new HashMap<String, Integer>();
+        
+        resultMap.put("髙橋", 90);
+        resultMap.put("中村", 70);
+        resultMap.put("佐藤", 0);
 
-        resultMap.put("髙橋",90);
-        resultMap.put("中村",80);
-        resultMap.put("佐藤",0);
-
-        String maxKey=null;
-        int maxValue=0;
+        String maxKey = null;
+        int maxValue = 0;
         for (Map.Entry<String, Integer> entry : resultMap.entrySet()) {
             if (entry.getValue() > maxValue) {
                 maxKey = entry.getKey();
@@ -19,30 +19,22 @@ public class Main {
             }
         }
 
-        String minKey=null;
-        int minValue=maxValue;
+        String minKey = null;
+        int minValue = maxValue;
         for (Map.Entry<String, Integer> entry : resultMap.entrySet()) {
             if (entry.getValue() < minValue) {
                 minKey = entry.getKey();
                 minValue = entry.getValue();
             }
         }
-        
-      int magnification;
+
+        int magnification;
         try {
-            magnification = maxValue/minValue;
-                    System.out.println("今回一番高い点数の" +maxKey+"さんは今回一番低い点数の"+minKey+"さんの"+ magnification + "倍の点数でした。");
-            } catch(Exception e){
-                    System.out.println("エラーがおきました。");
-                }
-            }
-          }
+            magnification = maxValue / minValue;
+            System.out.println("今回一番高い点数の" + maxKey + "さんは今回一番低い点数の" + minKey + "さんの" + magnification + "倍の点数でした。");
+        } catch (Exception e) {
+            System.out.println("エラーがおきました。");
+        }
+    }
 
-
-
-
-
-
-
-
-
+}
